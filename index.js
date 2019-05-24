80eda5de8a47fdb727a72a8d65d72238d18678e3
luizfonsecaa <luiz.otavio@take5.com.br>
2019-05-24 17:42:44 -0300

    Correções

--- index.js
+++ index.js
@@ -12,32 +12,74 @@ var Jimp = require('jimp');
  * pathSave:       Local a onde salvar a imagem
  */
 
-var backGroundImg = "#1f71b9"
+
+
+
+var backgroundImg = "#1f71b9"
 var logocomFundo = 'logocomFundo.png'
 
 let iosComFundo = [
-	{tamanhoLogoY: 57,tamanhoLogoX: 57,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:57,backgroundImgX:57,pathSave: 'caminho/icon.png'},
-	{tamanhoLogoY: 114,tamanhoLogoX: 114,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:57,backgroundImgX:57,pathSave: 'caminho/icon@2x.png'},
-	{tamanhoLogoY: 40,tamanhoLogoX: 40,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:40,backgroundImgX:40,pathSave: 'caminho/icon-20@2x.png'},
-	{tamanhoLogoY: 60,tamanhoLogoX: 60,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:60,backgroundImgX:60,pathSave: 'caminho/icon-20@3x.png'},
-	{tamanhoLogoY: 48,tamanhoLogoX: 48,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:48,backgroundImgX:48,pathSave: 'caminho/icon-24@2x.png'},
-	{tamanhoLogoY: 48,tamanhoLogoX: 48,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:48,backgroundImgX:48,pathSave: 'caminho/icon-27.5@2x.png'},
-	{tamanhoLogoY: 55,tamanhoLogoX: 55,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:55,backgroundImgX:55,pathSave: 'caminho/icon-27.png'},
-	{tamanhoLogoY: 29,tamanhoLogoX: 29,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:29,backgroundImgX:29,pathSave: 'caminho/icon-29.png'},
-	{tamanhoLogoY: 58,tamanhoLogoX: 58,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:58,backgroundImgX:58,pathSave: 'caminho/icon-29@2x.png'},
-	{tamanhoLogoY: 87,tamanhoLogoX: 87,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:87,backgroundImgX:87,pathSave: 'caminho/icon-29@3x.png'},
-	{tamanhoLogoY: 88,tamanhoLogoX: 88,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:88,backgroundImgX:88,pathSave: 'caminho/icon-44@2x.png'},
-	{tamanhoLogoY: 72,tamanhoLogoX: 72,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:72,backgroundImgX:72,pathSave: 'caminho/icon-72.png'},
-	{tamanhoLogoY: 144,tamanhoLogoX: 144,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:144,backgroundImgX:144,pathSave: 'caminho/icon-72@2x.png'},
-	{tamanhoLogoY: 76,tamanhoLogoX: 76,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:76,backgroundImgX:76,pathSave: 'caminho/icon-76.png'},
-	{tamanhoLogoY: 172,tamanhoLogoX: 172,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:172,backgroundImgX:172,pathSave: 'caminho/icon-86@2x.png'},
-	{tamanhoLogoY: 196,tamanhoLogoX: 196,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:196,backgroundImgX:196,pathSave: 'caminho/icon-98@2x.png'},
-	{tamanhoLogoY: 1024,tamanhoLogoX: 1024,imgMode: logocomFundo,hasBackground: true,backgroundImg:backGroundImg,backgroundImgY:1024,backgroundImgX:1024,pathSave: 'caminho/icon-1024.png'},
+	{tamanhoLogoY: 57,tamanhoLogoX: 57,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:57,backgroundImgX:57,pathSave: 'caminho/icon.png'},
+	{tamanhoLogoY: 114,tamanhoLogoX: 114,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:114,backgroundImgX:114,pathSave: 'caminho/icon@2x.png'},
+	{tamanhoLogoY: 40,tamanhoLogoX: 40,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:40,backgroundImgX:40,pathSave: 'caminho/icon-20@2x.png'},
+	{tamanhoLogoY: 60,tamanhoLogoX: 60,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:60,backgroundImgX:60,pathSave: 'caminho/icon-20@3x.png'},
+	{tamanhoLogoY: 48,tamanhoLogoX: 48,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:48,backgroundImgX:48,pathSave: 'caminho/icon-24@2x.png'},
+	{tamanhoLogoY: 48,tamanhoLogoX: 48,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:48,backgroundImgX:48,pathSave: 'caminho/icon-27.5@2x.png'},
+	{tamanhoLogoY: 55,tamanhoLogoX: 55,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:55,backgroundImgX:55,pathSave: 'caminho/icon-27.png'},
+	{tamanhoLogoY: 29,tamanhoLogoX: 29,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:29,backgroundImgX:29,pathSave: 'caminho/icon-29.png'},
+	{tamanhoLogoY: 58,tamanhoLogoX: 58,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:58,backgroundImgX:58,pathSave: 'caminho/icon-29@2x.png'},
+	{tamanhoLogoY: 87,tamanhoLogoX: 87,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:87,backgroundImgX:87,pathSave: 'caminho/icon-29@3x.png'},
+	{tamanhoLogoY: 88,tamanhoLogoX: 88,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:88,backgroundImgX:88,pathSave: 'caminho/icon-44@2x.png'},
+	{tamanhoLogoY: 72,tamanhoLogoX: 72,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:72,backgroundImgX:72,pathSave: 'caminho/icon-72.png'},
+	{tamanhoLogoY: 144,tamanhoLogoX: 144,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:144,backgroundImgX:144,pathSave: 'caminho/icon-72@2x.png'},
+	{tamanhoLogoY: 76,tamanhoLogoX: 76,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:76,backgroundImgX:76,pathSave: 'caminho/icon-76.png'},
+	{tamanhoLogoY: 172,tamanhoLogoX: 172,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:172,backgroundImgX:172,pathSave: 'caminho/icon-86@2x.png'},
+	{tamanhoLogoY: 196,tamanhoLogoX: 196,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:196,backgroundImgX:196,pathSave: 'caminho/icon-98@2x.png'},
+	{tamanhoLogoY: 1024,tamanhoLogoX: 1024,imgMode: logocomFundo,hasBackground: true,backgroundImg:backgroundImg,backgroundImgY:1024,backgroundImgX:1024,pathSave: 'caminho/icon-1024.png'},
 ]
 
+iosComFundo.map(foto => {
+	// verifico se ele precisa de um background
+	if(foto.hasBackground){
+		// verificar se ele tem uma img de background padrao
+		if(/^[a-zA-Z0-9-_\.]+\.(jpg|gif|png)$/.test(foto.backgroundImg)){
+			// Arruma o tamanho do backgraund
+			Jimp.read(foto.backgroundImg)
+			.then(img1 =>{
+				img1.resize(foto.backgroundImgX, foto.backgroundImg) // resize
+				Jimp.read(foto.imgMode)
+				.then(img2 => {
+					img2.resize(foto.tamanhoLogoY, foto.tamanhoLogoX)
+					img1.composite(img2,0,0)
+					.write(foto.pathSave, e => {console.log('foi')})
+				})
+				.catch(erro => console.log(erro))
+			.catch(erro => console.log(erro))
+			})
+		}else{
+			// crio uma imagem de background com a cor passada como parametro
+			new Jimp(foto.backgroundImgY, foto.backgroundImgX, foto.backgroundImg,  (err, image) => {
+				Jimp.read(foto.imgMode)
+				.then(img => {
+					img.resize(foto.tamanhoLogoY, foto.tamanhoLogoX)
+					image.composite(img, 0,0)
+					.write(foto.pathSave)
+				})
+			})
+		}
+	}else{
+		Jimp.resize(imgMode)
+		.then(img => {
+			img.resize(foto.tamanhoLogoX, foto.tamanhoLogoY)
+			.write(foto.pathSave)
+		})
+	}
+})
+
+
 // crio uma imagem de background com uma cor  e tamanho passado
-function createBackGround(backgroundImgY, backgroundImgX, backGroundImg, path){
-	new Jimp(backgroundImgX, backgroundImgY, backGroundImg, function (err, image) {
+function createBackGround(backgroundImgY, backgroundImgX, backgroundImg, path){
+	new Jimp(backgroundImgX, backgroundImgY, backgroundImg, function (err, image) {
 		if (err) console.log(err)
 		image.write(path, (err) => {
 			if(err){
@@ -60,7 +102,6 @@ function resizeImg(img, tamanhoLogoX, tamanhoLogoY, path){
 	.catch(err => {
 		console.error(err);
 	});
-
 }
 
 //junto duas imagens sendo uma de backgrund e a outro sendo o logo
@@ -76,34 +117,3 @@ function joinImg(img1,img2, mx, my, path){
 		console.log(e)
 	})
 }
-
-
-var teste =
-console.log(teste);
-
-
-
-iosComFundo.map(foto => {
-	// verifico se ele precisa de um background
-	if(foto.hasBackground){
-		// verificar se ele tem uma img de background padrao
-		if(/^[a-zA-Z0-9-_\.]+\.(jpg|gif|png)$/.test(foto.backGroundImg)){
-			// Arruma o tamanho do backgraund
-			resizeImg(foto.backgroundImg, foto.backgroundImgX, foto.backgroundImgY, 'background.png')
-		}else{
-			// crio uma imagem de background com a cor passada como parametro
-			var img1 = createBackGround(foto.backgroundImgY, foto.backgroundImgX, foto.backgroundImg, 'background.png')
-		}
-		// Corrigo o tamanho do logo
-		// var img1 = 'background.png'
-		// var img2 = 'logo.png'
-		resizeImg(foto.imgMode, foto.tamanhoLogoX, foto.tamanhoLogoY, 'logo.png')
-		// fazer as contas do mx e my
-		var marginX = (foto.backgroundImgX - foto.tamanhoLogoX)/2
-		var marginY = (foto.backgroundImgY - foto.tamanhoLogoY)/2
-		//junto as duas imagens
-		joinImg(img1, img2, marginX, marginY, foto.pathSave)
-	}else{
-		resizeImg(foto.logocomFundo, foto.tamanhoLogoX, foto.tamanhoLogoY, foto.pathSave)
-	}
-})
